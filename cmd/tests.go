c3fea985eae444c4c94b8c3a827e93898c18ca53
fedor.malyshkin <fedor.malyshkin@gmail.com>
2022-01-07 22:37:52 +0300

    Add inital structure

---
+++ cmd/tests.go
@@ -0,0 +1,7 @@
+package main
+
+import "coding-go/leetcode/p74"
+
+func main() {
+	p74.SearchMatrix([][]int{[]int{1}}, 1)
+}
---
+++ leetcode/p74/p74.go
@@ -0,0 +1,11 @@
+package p74
+
+import "fmt"
+
+func searchMatrix(matrix [][]int, target int) bool {
+	fmt.Println("123")
+	return false
+}
+func SearchMatrix(matrix [][]int, target int) bool {
+	return searchMatrix(matrix, target)
+}
